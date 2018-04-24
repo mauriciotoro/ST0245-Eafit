59bb2fb22b6d1a8c37c41eb3eba13cb548718e5d
mauriciotoro <mauriciotorob@gmail.com>
2018-04-24 08:18:29 -0500

    python

---
+++ talleres/taller12/Python/GraphAL.py
@@ -0,0 +1,16 @@
+class GraphAL:
+    def __init__(self, size):
+
+
+    def addArc(self, vertex, edge, weight):
+
+
+    def getSuccessors(self, vertice):
+
+
+    def getWeight(self, source, destination):
+
+
+    def __str__(self):
+
+
---
+++ talleres/taller12/Python/GraphAM.py
@@ -0,0 +1,17 @@
+class GraphAm:
+
+    def __init__(self, size):
+
+
+    def getEdges(self):
+
+
+    def getWeight(self, source, destination):
+
+
+    def addArc(self, source, destination, weight):
+
+    def getSuccessors(self, vertex):
+
+    def __str__(self):
+
