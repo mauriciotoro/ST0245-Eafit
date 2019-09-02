b85a5f33e4a899194637531723e133de02b09782
mauriciotoro <mauriciotorob@gmail.com>
2019-09-02 16:25:17 -0500

    mejoras al lab3

---
+++ laboratorios/lab03/codigo_estudiante/java/Laboratory3.java
@@ -0,0 +1,12 @@
+//Puntos opcionales
+public class Laboratory3 {
+  
+  public static void Pivote(List<Integer> lista) {
+      ...
+    }  
+    
+    
+  public static void neveras (Queue<String> solicitudes, Stack<String> neveras ){
+    ...     
+  }
+}
---
+++ laboratorios/lab03/codigo_estudiante/java/TecladoRoto.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class n here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class TecladoRoto{
+    //Punto 2.1
+    public static void teclado(String a){
+        LinkedList<Character>texto = new LinkedList<>();
+        ...
+    }
+}
