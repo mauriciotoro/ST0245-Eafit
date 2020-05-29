3eb53ada2f97b6573f0033ecb135d1f4cc3e9a17
mauriciotoro <mauriciotorob@gmail.com>
2020-05-29 07:52:10 -0500

    videos added

---
+++ talleres/taller07/spoiler/python/taller07.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+class Nodo():
+    def __init__(self, obj=None, nxt = None):
+        self.obj = obj
+        self.nxt = nxt
+        self.indx = indx
+
+    def __str__(self):
+        return "" + self.obj
+
+class Lsimple():
+    def __init__(self):
+        self.first_Node = None
+        self.last_Node = None
+
+    def __void(self):
+        return self.fisrt_Node == None
+
+    def insert(self, element):
+        if self.__void() == True:
+            self.fisrt_Node = self.last_Node = ____
+        else:
+            aux = self.last_Node
+            self.last_Node = aux.nxt = ____
+
+    def size(self):
+        ____
+
+    def remove(self):
+        aux = self.first_Node
+        while aux.nxt =! self.last_Node:
+            aux = aux.nxt
+        aux.nxt = None
+        self.last_Node = aux
+
+    def contains(self):
+        aux = self.fisrt_Node
+        while aux != None:
+            aux = aux.nxt
+	    _____
+
+    def insert(self, element, index):
+ 	_____
+
+    def remove(self, index):
+	_____
