6574e780848614608daa925654b6b668aa6d51ae
mauriciotoro <mauriciotorob@gmail.com>
2020-06-08 15:49:40 -0500

    datasets, spoiler en python, lectura, plantillas, menos obligatorios y mas opcionales

---
+++ laboratorios/lab01/spoiler/python/LongestCommonSubsequence.py
@@ -0,0 +1,11 @@
+
+def adn(chain1, chain2):
+  '''Function which find the longest common subsequence between two strings'''
+    if(len(chain1) == ???? or len(chain2)== ????):
+        return ????
+    if(chain1[len(chain1)-1] == chain2[len(chain2)-1]):
+        return adn(chain1[????], chain2[????) + ????
+    return max(adn(chain1[????], chain2), adn(chain1, chain2[????]))
+
+
+
---
+++ laboratorios/lab01/spoiler/python/Rectangles.py
@@ -0,0 +1,7 @@
+def formas(n):
+  '''Function which calculate the number of ways for filling a rectangle of 2xn with rectangles of 2x1'''
+  if(n <= ????):
+    return ????
+  return formas(????) + formas(????)
+
+
