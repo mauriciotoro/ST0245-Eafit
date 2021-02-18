8288f7f7684f5254b0417b8814acd94fff908d6c
mauriciotoro <mauriciotorob@gmail.com>
2021-02-18 10:47:50 -0500

    cambios en spoiler

--- talleres/taller04/spoiler/python/taller04.py
+++ talleres/taller04/spoiler/python/taller04.py
@@ -7,30 +7,9 @@ def array_generator(len):
     """List generator"""
     array = [0] * len
     for i in range(len):
-        array[i] = random.randrange(0,100)
+        array[i] = i
 
     return array
-
-def array_sum(array, sum = 0):
-    """Add the elements in the list"""
-    for element in array:
-        sum += element
-
-    return sum
-
-def multiplication_tables(n):
-    for i in range(1,n + 1):
-        for j in range(1,n + 1):
-            print (str(i) + " * " + str(j) + " = " + str(i*j))
-        print ("--------------------")
-
-def insertion_sort(list):
-    for index in range(len(list)):
-        for j in range(index,0,-1):
-            if list[j-1] > list[j]:
-                tmp = list[j]
-                list[j] = list[j-1]
-                list[j-1] = tmp
 
 def arrayMax(arr):
     return arrayMax_aux(arr, 0, 0)
